935e9826f0c22795882cd51b2d5b7ad8ae85611d
wanxun123 <wanxun0605@126.com>
2018-10-11 14:36:59 +0800

    init

---
+++ src/test/java/com/example/demo/LamdaTest.java
@@ -0,0 +1,11 @@
+package com.example.demo;
+
+import sun.awt.geom.AreaOp;
+
+import java.util.List;
+
+public class LamdaTest {
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}
